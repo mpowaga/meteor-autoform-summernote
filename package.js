7f41cc4e3f2cbf2e2147f83b5d554167e53d1440
Michal Powaga <michalpowaga13@gmail.com>
2014-12-14 15:32:50 +0000

    Bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mpowaga:autoform-summernote',
   summary: 'Summernote editor for aldeed:autoform',
-  version: '0.2.1',
+  version: '0.2.2',
   git: 'https://github.com/mpowaga/meteor-autoform-summernote'
 });
 
